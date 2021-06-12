be3a5bca79e745e1c1f1883a69607f4d5f49472e
GroovinChip <groovinchip@gmail.com>
2021-06-12 11:35:55 -0400

    Add bitsdojo_window

--- example/lib/apps/desktop/screens/home.dart
+++ example/lib/apps/desktop/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/widgets.dart';
 import 'package:macos_ui/macos_ui.dart';
+import 'package:pokemon_tcg/pokemon_tcg.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -9,8 +11,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final api = PokemonTcgApi(apiKey: '168a0f54-2d37-4070-b145-77ac370b0b42');
+  late final paginatedCardsAll = PaginatedPokemonCards([], api);
+  late final paginatedCardsSwsh5 = PaginatedPokemonCards([], api);
+
   @override
   Widget build(BuildContext context) {
-    return MacosScaffold();
+    return MacosScaffold(
+      sidebar: Sidebar(
+        builder: (context, scrollController) {
+          return ListView();
+        },
+        minWidth: 200,
+        isResizable: false,
+      ),
+      children: [
+        ContentArea(
+          builder: (context, scrollController) => Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FutureBuilder<List<PokemonCard>>(
+                  future: api.getCards(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: ProgressCircle(),
+                      );
+                    } else {
+                      return Text(
+                        '${snapshot.data!.length} cards'
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
