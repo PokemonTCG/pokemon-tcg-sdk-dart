d463fe9b4e2dd90854cd743dbf39a9fb9d261afd
Reuben Turner <groovinchip@gmail.com>
2021-06-12 19:26:47 -0400

    Show cards

--- example/lib/apps/desktop/screens/home.dart
+++ example/lib/apps/desktop/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:pokemon_tcg/pokemon_tcg.dart';
@@ -14,41 +16,69 @@ class _HomeState extends State<Home> {
   late final paginatedCardsAll = PaginatedPokemonCards([], api);
   late final paginatedCardsSwsh5 = PaginatedPokemonCards([], api);
 
+  late Future<List<PokemonCard>> latestSet = api.getCardsForSet('swsh5');
+
+  int pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MacosScaffold(
-      sidebar: Sidebar(
-        builder: (context, scrollController) {
-          return ListView();
-        },
-        minWidth: 200,
-        isResizable: false,
-      ),
-      children: [
-        ContentArea(
-          builder: (context, scrollController) => Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FutureBuilder<List<PokemonCard>>(
-                  future: api.getCards(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: ProgressCircle(),
-                      );
-                    } else {
-                      return Text(
-                        '${snapshot.data!.length} cards'
-                      );
-                    }
-                  },
+    return MoveWindow(
+      child: MacosScaffold(
+        sidebar: Sidebar(
+          builder: (context, scrollController) {
+            return SidebarItems(
+              currentIndex: pageIndex,
+              onChanged: (value) {
+                setState(() => pageIndex = value);
+              },
+              items: [
+                SidebarItem(
+                  label: Text('Latest Set'),
                 ),
               ],
+            );
+          },
+          minWidth: 200,
+          isResizable: false,
+        ),
+        children: [
+          ContentArea(
+            builder: (context, scrollController) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FutureBuilder<List<PokemonCard>>(
+                    future: latestSet,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: ProgressCircle(),
+                        );
+                      } else {
+                        final size = MediaQuery.of(context).size;
+                        return Expanded(
+                          child: GridView.builder(
+                            itemCount: snapshot.data!.length,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                            ),
+                            itemBuilder: (context, index) {
+                              return Image.network(
+                                snapshot.data![index].images.small,
+                              );
+                            },
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
