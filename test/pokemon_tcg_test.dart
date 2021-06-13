90f4740d470a3988733c7c170609b0745ca0d403
GroovinChip <groovinchip@gmail.com>
2021-06-12 20:52:52 -0400

    test cards.json length

--- test/pokemon_tcg_test.dart
+++ test/pokemon_tcg_test.dart
@@ -2,22 +2,33 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pokemon_tcg/src/models/card.dart';
 import 'package:pokemon_tcg/src/models/set.dart';
 import 'package:pokemon_tcg/src/type_aliases.dart';
 
 void main() {
   File setsFile = File('test/data/sets.json');
   File setFile = File('test/data/set.json');
+  File cardsFile = File('test/data/cards.json');
   late CardSet battleStyles;
   CardSets allSets = [];
+  List<PokemonCard> cards = [];
 
   setUp(() async {
     final set = await setFile.readAsString();
     final sets = await setsFile.readAsString();
+    final _cards = await cardsFile.readAsString();
     battleStyles = CardSet.fromJson(jsonDecode(set)['data']);
+
     List setsList = jsonDecode(sets)['data'];
     setsList.forEach((element) {
       allSets.add(CardSet.fromJson(element));
+    });
+
+    List cardsList = jsonDecode(_cards)['data'];
+    cards.clear();
+    cardsList.forEach((element) {
+      cards.add(PokemonCard.fromJson(element));
     });
   });
 
@@ -29,5 +40,9 @@ void main() {
     test('Get sets', () async {
       expect(battleStyles.id, 'swsh5');
     });
+  });
+
+  test('Cards length', () {
+    expect(cards.length, 250);
   });
 }
