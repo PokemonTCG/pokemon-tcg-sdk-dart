402cef651d61287e9d986026c6d22e0782ea2bf0
Reuben Turner <groovinchip@gmail.com>
2021-06-14 14:44:54 -0400

    Export models (#6)
    * Add getTypes() function
    
    * Add getSubtypes() function
    
    * Correct file name
    
    * Add getSupertypes() function
    
    * Add getRarities() function
    
    * bump version, CHANGELOG.md, run dartfmt
    
    * Version 0.3.0
    
    * Dartfmt
    
    * fix issue tracker url
    
    * 1.0.0
    
    * Remove API key
    
    * test cards.json length
    
    * Refactor tests
    
    * Export models

--- lib/pokemon_tcg.dart
+++ lib/pokemon_tcg.dart
@@ -1,4 +1,12 @@
 library pokemon_tcg;
 
+export 'src/models/card.dart';
+export 'src/models/elemental_type.dart';
+export 'src/models/images.dart';
+export 'src/models/legalities.dart';
+export 'src/models/rarity.dart';
+export 'src/models/set.dart';
+export 'src/models/subtype.dart';
+export 'src/models/supertype.dart';
 export 'src/paginated_pokemon_cards.dart';
 export 'src/pokemon_tcg.dart';
